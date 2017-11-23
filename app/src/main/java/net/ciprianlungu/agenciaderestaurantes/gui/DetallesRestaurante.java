724c02e72ea33a1add5d36c26936d8d04bbadbfc
Lionkyde <lionkyde@yahoo.com>
2017-11-23 05:28:31 +0100

    Version 3.2 Botones siguiente/anterior funcionales.

--- app/src/main/java/net/ciprianlungu/agenciaderestaurantes/gui/DetallesRestaurante.java
+++ app/src/main/java/net/ciprianlungu/agenciaderestaurantes/gui/DetallesRestaurante.java
@@ -1,5 +1,6 @@
 package net.ciprianlungu.agenciaderestaurantes.gui;
 
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
@@ -9,52 +10,103 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.ciprianlungu.agenciaderestaurantes.R;
+import net.ciprianlungu.agenciaderestaurantes.persistencia.DatabaseHelper;
+import net.ciprianlungu.agenciaderestaurantes.persistencia.GestorBBDDRestaurantes;
 
 import org.w3c.dom.Text;
 
 import java.io.File;
 
 public class DetallesRestaurante extends AppCompatActivity {
+    GestorBBDDRestaurantes gr = new GestorBBDDRestaurantes(this);
+    Cursor cursor;
+    TextView tvNombre;
+    ImageView imagenView;
+    TextView tvTelefono;
+    TextView tvDireccion;
+    TextView tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_restaurante);
         getSupportActionBar().setHomeButtonEnabled(true);
+        cursor = gr.getRestaurantes();
 
-        TextView tvNombre = (TextView)findViewById(R.id.detalles_tvNombre);
-        ImageView imagenView = (ImageView)findViewById(R.id.detalles_imagen_restaurante);
-        TextView tvTelefono = (TextView)findViewById(R.id.detalles_tvTelefono);
-        TextView tvDireccion = (TextView)findViewById(R.id.detalles_tvDireccion);
-        TextView tvEmail = (TextView)findViewById(R.id.detalles_tvEmail);
+        tvNombre = (TextView)findViewById(R.id.detalles_tvNombre);
+        imagenView = (ImageView)findViewById(R.id.detalles_imagen_restaurante);
+        tvTelefono = (TextView)findViewById(R.id.detalles_tvTelefono);
+        tvDireccion = (TextView)findViewById(R.id.detalles_tvDireccion);
+        tvEmail = (TextView)findViewById(R.id.detalles_tvEmail);
 
         //RECOGER LOS VARIABLES PASADOS AL INTENT
         Bundle bundle = getIntent().getExtras();
 
-        String nombre = String.valueOf(bundle.get("nombre")); //campo nombre
-        String ruta_imagen = String.valueOf(bundle.get("imagen")); //ruta imagen
-        int telefono = Integer.parseInt(String.valueOf(bundle.get("telefono")));
-        String direccion = String.valueOf(bundle.get("direccion"));
-        String email = String.valueOf(bundle.get("email"));
+        int position = (int) bundle.get("position"); //campo nombre
 
 
-        tvNombre.setText(nombre);
-        tvTelefono.setText(String.valueOf(telefono));
-        tvDireccion.setText(direccion);
-        tvEmail.setText(email);
+        if(cursor != null){
+            cursor.moveToPosition(position);
 
+            tvNombre.setText(cursor.getString(1));
+            tvTelefono.setText(cursor.getString(3));
+            tvDireccion.setText(cursor.getString(4));
+            tvEmail.setText(cursor.getString(5));
 
-        //CARGA DE IMAGEN A PARTIR DEL EXTRA DE LA RUTA PASADA
-        File imagen = new File(ruta_imagen);
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        Bitmap bitmap = BitmapFactory.decodeFile(imagen.getAbsolutePath(),bmOptions);
-        imagenView.setImageBitmap(bitmap);
-
+            //CARGA DE IMAGEN A PARTIR DEL EXTRA DE LA RUTA PASADA
+            File imagen = new File(cursor.getString(2));
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            Bitmap bitmap = BitmapFactory.decodeFile(imagen.getAbsolutePath(),bmOptions);
+            imagenView.setImageBitmap(bitmap);
+        }
     }
+
+    public void siguiente(View v){
+        Log.d("cursor","position:"+cursor.getPosition()+" count:"+cursor.getCount());
+        if(cursor.getPosition() < (cursor.getCount()-1)){
+            if(cursor != null && !cursor.isAfterLast()){
+                cursor.moveToNext();
+
+                tvNombre.setText(cursor.getString(1));
+                tvTelefono.setText(cursor.getString(3));
+                tvDireccion.setText(cursor.getString(4));
+                tvEmail.setText(cursor.getString(5));
+
+                //CARGA DE IMAGEN A PARTIR DEL EXTRA DE LA RUTA PASADA
+                File imagen = new File(cursor.getString(2));
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                Bitmap bitmap = BitmapFactory.decodeFile(imagen.getAbsolutePath(),bmOptions);
+                imagenView.setImageBitmap(bitmap);
+            }
+        }
+    }
+    public void anterior(View v){
+        if(cursor.getPosition() > 0){
+            if(cursor != null && !cursor.isAfterLast()){
+                cursor.moveToPrevious();
+
+                tvNombre.setText(cursor.getString(1));
+                tvTelefono.setText(cursor.getString(3));
+                tvDireccion.setText(cursor.getString(4));
+                tvEmail.setText(cursor.getString(5));
+
+                //CARGA DE IMAGEN A PARTIR DEL EXTRA DE LA RUTA PASADA
+                File imagen = new File(cursor.getString(2));
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                Bitmap bitmap = BitmapFactory.decodeFile(imagen.getAbsolutePath(),bmOptions);
+                imagenView.setImageBitmap(bitmap);
+            }
+        }
+    }
+    public void llamar(View v){
+        //TODO PRIMERO HAY QUE PEDIRLE LOS PERMISOS PARA PODER HACER LLAMADA. CREAMOS BOOLEANO DE PERMISO Y LUEGO HACEMOS LLAMADA
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
--- app/src/main/java/net/ciprianlungu/agenciaderestaurantes/gui/PrincipalActivity.java
+++ app/src/main/java/net/ciprianlungu/agenciaderestaurantes/gui/PrincipalActivity.java
@@ -38,27 +38,11 @@ public class PrincipalActivity extends AppCompatActivity {
         lvListaRestaurantes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Cursor cursor;
-                cursor = gr.getRestaurante(l);
-
-                if(cursor != null){
-                    cursor.moveToFirst();
-
-                    Log.d("cursor", String.valueOf(cursor.getInt(cursor.getColumnIndex("_id"))));
-                    Log.d("cursor",cursor.getString(cursor.getColumnIndex("nombre")));
-                    Log.d("cursor",cursor.getString(cursor.getColumnIndex("ruta_imagen")));
-                    Log.d("cursor", String.valueOf(cursor.getInt(cursor.getColumnIndex("telefono"))));
-                    Log.d("cursor", cursor.getString(cursor.getColumnIndex("direccion")));
-                    Log.d("cursor", cursor.getString(cursor.getColumnIndex("email")));
 
                     Intent intent = new Intent(PrincipalActivity.this,DetallesRestaurante.class);
-                    intent.putExtra("nombre",cursor.getString(cursor.getColumnIndex("nombre")));
-                    intent.putExtra("imagen",cursor.getString(cursor.getColumnIndex("ruta_imagen")));
-                    intent.putExtra("telefono",String.valueOf(cursor.getInt(cursor.getColumnIndex("telefono"))));
-                    intent.putExtra("direccion",cursor.getString(cursor.getColumnIndex("direccion")));
-                    intent.putExtra("email",cursor.getString(cursor.getColumnIndex("email")));
+                    intent.putExtra("position",i);
                     startActivity(intent);
-                }
+
             }
         });
 
